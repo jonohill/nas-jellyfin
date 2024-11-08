3ec2f10480c9f7547e24753e46014cdad56bd23c
Jono Hill <jono@hillnz.com>
2024-11-08 13:13:54 +1300

    Add some deps

---
+++ root/await.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+
+for arg in "$@"; do
+
+    if [ "$arg" = "caddy" ]; then
+        while ! curl --silent --fail https://id.jottacloud.com:443 >/dev/null; do
+            echo "Waiting for caddy..."
+            sleep 1
+        done
+    fi
+
+    if [ "$1" = "rclone" ]; then
+        while [ ! "$(ls -A "$RCLONE_MOUNT_DIR")" ]; do
+            echo "Waiting for rclone mount..."
+            sleep 1
+        done
+    fi
+
+done
